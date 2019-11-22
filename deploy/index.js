b4edecbeaaab748a04107b8d75ccba6266f49773
CH <CH@CH.com>
2019-11-22 10:07:11 +0800

    1

--- deploy/index.js
+++ deploy/index.js
@@ -3,7 +3,7 @@ const chalk = require("chalk");
 const path = require("./lib/path");
 const file = require("./lib/file");
 
-const sshAddress = "false";
+const sshAddress = "5dd3";
 
 require("shelljs/global");
 
